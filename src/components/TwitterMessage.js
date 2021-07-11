39106f7801398dd5ff4aee0481227c1f979fd0ce
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-11 17:05:11 +1000

    Lab completed

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,40 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     message: '',
+  //     charsAvailable: this.props.maxChars
+  //   };
+  // }
 
-    this.state = {};
+//THE ABOVE WON'T WORK, WHY?
+
+  state = {
+    message: '',
+    charsAvailable: this.props.maxChars
   }
 
+  handleChange(event) {
+    let remainingChars = this.props.maxChars
+    this.setState({
+      message: event.target.value,
+      charsAvailable: remainingChars - event.target.value.length
+    })
+    // console.log(this.state.message.length)
+    // console.log(this.state.charsAvailable)
+  }
+
+
   render() {
+    // console.log(this.props.maxChars)
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event=>this.handleChange(event)} value={this.state.message}/>
+        <p>Remaining characters: {this.state.charsAvailable}</p>
       </div>
     );
   }
